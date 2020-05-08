11fdaa0644fa08e4449381b1e7fb230e790c67a5
Abhishek Srivastava <srivastava.abh@husky.neu.edu>
2020-05-08 00:42:59 -0400

    Updating Number of epochs

--- FacialKeyPointDetection/util.py
+++ FacialKeyPointDetection/util.py
@@ -4,7 +4,7 @@ import torch
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 BATCH_SIZE = 32
 NUM_CLASSES = 30
-NUM_EPOCHS = 1
+NUM_EPOCHS = 20
 RANGE = 5
 
 def convert_to_matrix(data):
